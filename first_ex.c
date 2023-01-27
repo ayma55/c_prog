dd20af73eac6a5d3fd861b0a3520a1013e57650b
ayma5 <aymenhaddar91@gmail.com>
2023-01-27 18:45:10 +0100

    add a header file

--- first_ex.c
+++ first_ex.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 
+
+int func1(int a,int b)
+{
+    int sum = a + b;
+    return sum;
+}
+
 int main()
 {
     char str[10];
+    int a,b;
+    printf("enter nbr1");
+    scanf("%d",&a);
+    printf("enter nbr2");
+    scanf("%d",&b);
     printf("enter the word: ");
     scanf("%s",str);
     printf("display the word",str);
---
+++ header_file.h
@@ -0,0 +1,11 @@
+#ifndef HEADERFILE_H
+#define HEADERFILE_H
+
+// structure 
+
+
+// function declartion
+
+
+
+#endif //end of header file
